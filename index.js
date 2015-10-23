51553d77cb0da092354bfe194f6fcf702096bcb7
Lynxyss <LynxyssCZ@gmail.com>
2015-10-23 14:22:55 +0200

    stuf...

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ require('dotenv').load();
 var SensorsManager = require('./src/thermostat/SensorsManager');
 
 
-var manager = new SensorsManager({
-	callback: function (name, value) {
-		console.log(name, value);
-	}
-});
+var manager;
 
-manager.addSensor('DS18B20', 'DS18B20', {
-	interval: 5000
-});
+var clear = function() {
+	manager.stop();
+	process.exit();
+};
 
-manager.start();
+var start = function() {
+	manager = new SensorsManager({
+		callback: function (name, value) {
+			console.log(name, value);
+		}
+	});
+
+	manager.addSensor('DS18B20', 'DS18B20', {
+		interval: 5000
+	});
+
+	manager.start();
+};
+process.on('SIGINT', clear);
+
+start();

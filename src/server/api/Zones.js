4188c804bbad148cc400f9cbe0c2dba03ad7b947
Lynxyss <LynxyssCZ@gmail.com>
2015-11-02 16:46:03 +0100

    valid 0

--- src/server/api/Zones.js
+++ src/server/api/Zones.js
@@ -116,7 +116,7 @@ var endpoints = [
 				payload: {
 					name: Joi.string().min(5).max(50).required(),
 					sensors: Joi.array().items(Joi.string().required()).unique(),
-					priority: Joi.number().positive().precision(2).max(150).default(1)
+					priority: Joi.number().positive().valid(0).precision(2).max(150).default(1)
 				}
 			}
 		}

212089c6194389741b716f71ef4f2387af56bc89
Lynxyss <LynxyssCZ@gmail.com>
2015-11-13 18:37:53 +0100

    Some tests boys

--- src/db/index.js
+++ src/db/index.js
@@ -1,27 +1,19 @@
 var Knex = require('knex');
 var Bookshelf = require('bookshelf');
-var mkdirp = require('mkdirp');
-var path = require('path');
 
 var FlemDB = module.exports = {
 	bookshelf: null,
 	models: null,
 	config: function(options, next) {
-		mkdirp(path.dirname(options.knexFile.connection.filename), function(err) {
-			if (err) {
-				return next(err);
-			}
+		var knex = Knex(options.knexFile);
+		var bookshelf = FlemDB.bookshelf = Bookshelf(knex);
+		bookshelf.plugin(['virtuals', 'registry', 'visibility', 'bookshelf-camelcase']);
+		FlemDB.models = require('./models')(bookshelf);
 
-			var knex = Knex(options.knexFile);
-			var bookshelf = FlemDB.bookshelf = Bookshelf(knex);
-			bookshelf.plugin(['virtuals', 'registry', 'visibility', 'bookshelf-camelcase']);
-			FlemDB.models = require('./models')(bookshelf);
-
-			knex.migrate.latest()
-				.then(function() {
-					next(null);
-				});
-		});
+		knex.migrate.latest()
+			.then(function() {
+				next(null);
+			});
 	},
 	getModel: function(name) {
 		return FlemDB.models[name].Model;
